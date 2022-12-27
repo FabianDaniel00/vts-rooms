dff9e71c890a265bfd0af793cbd477872694c3c4
FabianDaniel00 <danielfabian090@gmail.com>
2022-12-27 18:53:29 +0100

    init

---
+++ js.js
@@ -0,0 +1,79 @@
+import rooms from './rooms.js';
+
+const inputForm = document.getElementById('input-form')
+const input = document.getElementById('input');
+const floorParagraph = document.getElementById('floor');
+const roomParagraph = document.getElementById('room');
+const floorCanvas = document.getElementById('floor-canvas');
+const floorCanvasContext = floorCanvas.getContext('2d');
+let interval = null;
+
+inputForm.onsubmit = (formSubmitEvent) => {
+  formSubmitEvent.preventDefault();
+
+  const room = rooms.find((room) => String(room.id) === input.value.toLowerCase());
+
+  if (!room) {
+    input.focus();
+    return alert('This room not found!');
+  }
+
+  clearInterval(interval);
+  const { floor: roomFloor, path: roomPath } = room;
+  const floorImageSrc = `./floors/floor_${roomFloor}.png`;
+  const floorImage = new Image();
+
+  floorParagraph.innerHTML = `Floor: ${roomFloor}`;
+  roomParagraph.innerHTML = `Room: ${room.id}`;
+
+  floorCanvas.style.backgroundImage = `url(${floorImageSrc})`;
+
+  floorImage.onload = () => {
+    const { width, height } = floorImage;
+
+    floorCanvasContext.canvas.width = width;
+    floorCanvasContext.canvas.height = height;
+
+    let roomPathIndex = 0;
+    let roomPathX = roomPath[0].x;
+    let roomPathY = roomPath[0].y;
+
+    const draw = () => {
+      floorCanvasContext.clearRect(0, 0, width, height);
+
+      floorCanvasContext.strokeStyle = '#0093DD';
+      floorCanvasContext.lineWidth = 3;
+      floorCanvasContext.beginPath();
+      floorCanvasContext.moveTo(roomPath[0].x, roomPath[0].y);
+      for (let i = 1; i < roomPath.length; i++) {
+        floorCanvasContext.lineTo(roomPath[i].x, roomPath[i].y);
+      }
+      floorCanvasContext.stroke();
+
+      floorCanvasContext.fillStyle = "#FF0000";
+      floorCanvasContext.beginPath();
+      floorCanvasContext.arc(roomPathX, roomPathY, 5, 0, 2 * Math.PI);
+      floorCanvasContext.fill();
+
+      if (roomPathX < roomPath[roomPathIndex + 1].x) {
+        roomPathX += 1;
+      } else if (roomPathY < roomPath[roomPathIndex + 1].y) {
+        roomPathY += 1;
+      } else if (roomPathX > roomPath[roomPathIndex + 1].x) {
+        roomPathX -= 1;
+      } else if (roomPathY > roomPath[roomPathIndex + 1].y) {
+        roomPathY -= 1;
+      } else {
+        roomPathIndex++;
+        if (roomPathIndex >= roomPath.length - 1) {
+          roomPathIndex = 0;
+          roomPathX = roomPath[0].x;
+          roomPathY = roomPath[0].y;
+        }
+      }
+    }
+
+    interval = setInterval(draw, 15);
+  };
+  floorImage.src = floorImageSrc;
+};
